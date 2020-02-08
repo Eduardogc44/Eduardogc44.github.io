c7f73098ed7729ac9a8e9f620ac40ac4c3d0d62f
Eduardogc44 <52419006+Eduardogc44@users.noreply.github.com>
2020-02-08 11:01:41 -0500

    comm3

---
+++ wwwroot/js/Generos.js
@@ -0,0 +1,23 @@
+﻿
+class Generos {
+
+    RegistrarGenero() {
+        $.post(
+            "GetGeneros",
+            $('.formGenero').serialize(),
+            (response) => {
+                try {
+                    var item = JSON.parse(response);
+                    if (Item.Code == "Done") {
+                        window.location.href = "Index";
+                    } else {
+                        document.getElementById("mensaje").innerHTML = item.Description;
+                    }
+                } catch (e) {
+                    document.getElementById("mensaje").innerHTML = response;
+                }                
+                console.log(response);
+            }
+        );
+    }
+}
